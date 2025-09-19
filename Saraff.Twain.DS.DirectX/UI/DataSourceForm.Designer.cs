0160a49c7014b3ac38a17a37642c1248978a0a6a
1amnoob <pytp@outlook.com>
2025-09-19 17:48:49 -0600

    Update DataSourceForm.Designer.cs

--- Saraff.Twain.DS.DirectX/UI/DataSourceForm.Designer.cs
+++ Saraff.Twain.DS.DirectX/UI/DataSourceForm.Designer.cs
@@ -253,6 +253,7 @@ namespace Saraff.Twain.DS.DirectX.UI {
             this.Name = "DataSourceForm";
             this.Text = "DataSourceForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TopMost = true;
             //this.splitContainer1.Panel1.ResumeLayout(false);
             //this.splitContainer1.Panel2.ResumeLayout(false);
             //((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
